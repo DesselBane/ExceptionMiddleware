87dce3d6979ecf1c836ae0e471b84714a2a6b485
Dessel Bane <dessel.bane@gmx.de>
2018-02-19 11:14:50 +0100

    Added Extesion Method

---
+++ ExceptionMiddlewareExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace ExceptionMiddleware
+{
+    public static class ExceptionMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseExceptionMiddleware(this IApplicationBuilder app)
+        {
+            app.UseMiddleware<global::ExceptionMiddleware.ExceptionMiddleware>();
+
+            return app;
+        }
+    }
+}
