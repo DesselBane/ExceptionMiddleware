9a991c30a3ed9d3228e4ad4b00dfa8c8dd8e29e9
Dessel Bane <dessel.bane@gmx.de>
2018-03-12 13:23:06 +0100

    Added Unsupported Media Type Exception Upped NuGet Version

---
+++ UnsupportedMediaTypeException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExceptionMiddleware
+{
+    public class UnsupportedMediaTypeException : InvalidRestOperationException
+    {
+        public UnsupportedMediaTypeException(string message, Guid customErrorCode, Exception innerException = null)
+            : base(message, customErrorCode, innerException) { }
+
+        public override int ResponseCode => 415;
+    }
+}
