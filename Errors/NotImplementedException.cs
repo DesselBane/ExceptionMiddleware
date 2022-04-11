2a575228e33e348484d4ace14360136518cc15ba
Jan Morawietz <jan.morawietz@paessler.com>
2022-04-11 14:52:07 +0200

    add NotImplementedException

---
+++ Errors/NotImplementedException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Net;
+
+namespace ExceptionMiddleware.Errors;
+
+public class NotImplementedException : InvalidRestOperationException
+{
+    public NotImplementedException(string message, string customErrorCode, Exception innerException = null) : base(message, customErrorCode, innerException)
+    {
+    }
+
+    public override int ResponseCode => (int)HttpStatusCode.NotImplemented;
+}
