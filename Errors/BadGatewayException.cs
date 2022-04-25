805f2f5118323c1f08b943bf1c0e486060335f1a
Jan Morawietz <jan.morawietz@paessler.com>
2022-04-25 12:00:49 +0200

    feat: Add BadGatewayException feat: Add GatewayTimeoutException chore: add publish profile

---
+++ Errors/BadGatewayException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Net;
+
+namespace ExceptionMiddleware.Errors;
+
+public class BadGatewayException : InvalidRestOperationException
+{
+    public BadGatewayException(Uri target, string customErrorCode, Exception innerException = null)
+        : this($"Failed to reach {target}.", customErrorCode, innerException)
+    {
+    }
+
+    public BadGatewayException(string message, string customErrorCode, Exception innerException = null) : base(message,
+        customErrorCode, innerException)
+    {
+    }
+
+    public override int ResponseCode => (int)HttpStatusCode.BadGateway;
+}
---
+++ Errors/GatewayTimeoutException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Net;
+
+namespace ExceptionMiddleware.Errors;
+
+public class GatewayTimeoutException : InvalidRestOperationException
+{
+    public GatewayTimeoutException(Uri target, TimeSpan timeout, string customErrorCode,
+        Exception innerException = null) : this(
+        $"Timed out waiting for a response from {target}. Waited for {timeout:o}", customErrorCode,
+        innerException)
+    {
+    }
+
+    public GatewayTimeoutException(string message, string customErrorCode, Exception innerException = null) : base(
+        message, customErrorCode, innerException)
+    {
+    }
+
+    public override int ResponseCode => (int)HttpStatusCode.GatewayTimeout;
+}
